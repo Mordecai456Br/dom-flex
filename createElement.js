4e3439c8d9f702e5e53a074cb8d2110932fe07f5
Mordecai456Br <gabrielseixas1@gmail.com>
2025-05-24 19:21:53 -0300

    addElement + loadElement
    Agora as funcoes sao divididas

--- createElement.js
+++ createElement.js
@@ -2,8 +2,8 @@ const content = document.querySelector('.content');
 // elements
 
 // color-selector and colors
-let backgroundColors = [["red",'CC3A3A'],["orange",'DD6033'],["yellow",'EDC63A'],["green",'94CC3A'],
-["blue",'3AADCC'],["dark-blue",'3A70CC'],["purple",'943ACC'],["pink",'CC3A94']];
+let backgroundColors = [["red", 'CC3A3A'], ["orange", 'DD6033'], ["yellow", 'EDC63A'], ["green", '94CC3A'],
+["blue", '3AADCC'], ["dark-blue", '3A70CC'], ["purple", '943ACC'], ["pink", 'CC3A94']];
 
 let colorSelected = 0;
 let elementBackground = [backgroundColors[colorSelected][1]];
@@ -29,15 +29,11 @@ quando for criar os elementos adicione na lista
 
 // elementos
 const newElementSelector = document.querySelector('.newElementSelector');
-newElementSelector.addEventListener('change',() => {
-    const newElementSelected = (document.querySelector('.newElementSelector').value).slipt(",");
-    console.log(newElementSelected);
-})
-let buttonArrayIndicator = parseInt(newElementSelector);
-console.log(buttonArrayIndicator);
+
 const elementos = ['div', ['p', 'h1', 'h2', 'h3', 'h4'], 'span', 'button'];
-let elementToBeCreated = [elementos[1][1], elementos[3],elementos[3]];
-let newElementInArray = 
+let elementToBeCreated = [elementos[1][1], elementos[3], elementos[3]];
+
+
 `let iTest=0;
 newListElement = document.createElement('li');
 newListElement.innerHTML = elementToBeCreated[iTest];
@@ -56,55 +52,95 @@ if (elementToBeCreated ===
 }
 let elementWithContent = ["ola", null, "eai"];
 
+let newElementContent = "balela";
+let newElementSelected = "";
+newElementSelector.addEventListener('change', () => {
+    newElementSelected = document.querySelector('.newElementSelector').value.split(",").map(Number);
+    let index0Selected = newElementSelected[0];
+    let index1Selected = newElementSelected[1];
+    
+    if (newElementSelected.length < 2) {
+        newElementSelected = elementos[index0Selected];
+    } else newElementSelected = elementos[index0Selected][index1Selected];
+    addElements(newElementSelected, newElementContent, elementBackground);
 
-`let element_content = ["ola este seria o conteudo", '<div style="background:#a6ff52; width:100%; height:20px "><div/>'];
-const novoElemento = document.createElement(elementToBeCreated);
+    console.log(newElementSelected);
+    console.log(elementToBeCreated);
+    });
+    `let element_content = ["ola este seria o conteudo", '<div style="background:#a6ff52; width:100%; height:20px "><div/>'];
+    const novoElemento = document.createElement(elementToBeCreated);
 novoElemento.innerHTML = element_content;
 content.append(novoElemento);`// essa pequena area tava recebendo a lista e era preparada apenas
-// para um 1 tava quebrando meu codigo 😒
+    // para um 1 tava quebrando meu codigo 😒
 
-/* primeiro aprendizado: quando a funcao espera 1 unico valor como string e vc retorna um array,
-ele so ira percorrer caso vc crie um for e nao um forEach. 
-Tava tentando adicionar elementos com createElement passando um array.
-*/
+    /* primeiro aprendizado: quando a funcao espera 1 unico valor como string e vc retorna um array,
+    ele so ira percorrer caso vc crie um for e nao um forEach. 
+    Tava tentando adicionar elementos com createElement passando um array.
+    */
 
-//minha ideia aqui consiste em garantir que o sistema tenha opcao de aceitar ou
-//nao elementos de conteudos vazios
-let allowNullContents = true;
-let applyBackground = true;
-function addElements(elementToBeCreated, element_content, elementBackground) {
-    for (let i = 0; i < elementToBeCreated.length; i++) {
-        const elementCreated = document.createElement(elementToBeCreated[i]);
+    //minha ideia aqui consiste em garantir que o sistema tenha opcao de aceitar ou
+    //nao elementos de conteudos vazios
+    let allowNullContents = true;
+    let applyBackground = true;
 
-        if (element_content[i]) {
-            elementCreated.innerHTML = element_content[i];
-            addToList(i);
+    function addElements(newElementSelected, element_content, elementBackground) {
+        let elementCreated = document.createElement(newElementSelected);
+        elementToBeCreated.push(newElementSelected);
+        let index = elementToBeCreated.length; // ja q ele é o novo elemento, pega o tamanho do array
+
+        if (newElementContent != null) {
+            elementCreated.innerHTML = newElementContent;
+            addToList(index);
             content.append(elementCreated);
         } else if (allowNullContents === false) {
-            console.log(`${elementToBeCreated[i]} or element[${i}] has not any content, and not created. You can
+            console.log(`${elementToBeCreated[index]} or element[${index}] has not any content, and not created. You can
 allow null contents setting allowNullContents = true`);
-            
+
         } else {
-            console.log(`${elementToBeCreated[i]} has not any content, but created anyway`);
+            console.log(`${elementToBeCreated[index]} has not any content, but created anyway`);
         }
-        if (elementBackground[i] && applyBackground === true){
-            elementCreated.style.background = `#${elementBackground[i]}`;
-            console.log(elementBackground[i])
-            console.log(`${elementToBeCreated[i]} element[${i}] has not any content, but created anyway`);
-            addToList(i);
+        if (elementBackground && applyBackground === true) {
+            elementCreated.style.background = `#${elementBackground}`;
+            console.log(elementBackground)
+            console.log(`${elementToBeCreated[index]} element[${index}] has not any content, but created anyway`);
+            addToList(index);
             content.append(elementCreated);
         }
-        
+        console.log(elementToBeCreated);
     }
 
-    console.log(elementToBeCreated);
-}
-function addToList (index){
-    const list = document.querySelector('.created-elements-list');
-    newListElement = document.createElement('li');
-    newListElement.innerHTML = `${elementToBeCreated[index]} | content: ${elementWithContent[index]}`;
-    list.append(newListElement);
-}
-// licao 2, sempre lembrar de colocar .length lidando com arrays
+    function loadElements(elementToBeCreated, element_content, elementBackground) {
+        for (let i = 0; i < elementToBeCreated.length; i++) {
+            const elementCreated = document.createElement(elementToBeCreated[i]);
 
-addElements(elementToBeCreated, elementWithContent,elementBackground);
+            if (element_content[i]) {
+                elementCreated.innerHTML = element_content[i];
+                addToList(i);
+                content.append(elementCreated);
+            } else if (allowNullContents === false) {
+                console.log(`${elementToBeCreated[i]} or element[${i}] has not any content, and not created. You can
+allow null contents setting allowNullContents = true`);
+
+            } else {
+                console.log(`${elementToBeCreated[i]} has not any content, but created anyway`);
+            }
+            if (elementBackground[i] && applyBackground === true) {
+                elementCreated.style.background = `#${elementBackground[i]}`;
+                console.log(elementBackground[i])
+                console.log(`${elementToBeCreated[i]} element[${i}] has not any content, but created anyway`);
+                addToList(i);
+                content.append(elementCreated);
+            }
+        }
+        console.log(elementToBeCreated);
+    }
+
+    function addToList(index) {
+        const list = document.querySelector('.created-elements-list');
+        newListElement = document.createElement('li');
+        newListElement.innerHTML = `${elementToBeCreated[index]} | content: ${elementWithContent[index]}`;
+        list.append(newListElement);
+    }
+    // licao 2, sempre lembrar de colocar .length lidando com arrays
+
+    loadElements(elementToBeCreated, elementWithContent, elementBackground);
