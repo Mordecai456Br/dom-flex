d1fb3385813975562fb3af55363ba373d1ef0f2b
Mordecai456Br <gabrielseixas1@gmail.com>
2025-05-27 23:49:54 -0300

    desafios

--- createElement2.js
+++ createElement2.js
@@ -14,10 +14,67 @@ function createNewElement(htmlElementWithIndex){
         id: newId,
         htmlElement: htmlElementWithIndex}
     elements.push(newElement)
-    elements.pop(0)
+    
 }
 createNewElement(htmlElements[3]);
 createNewElement(htmlElements[3]);
 createNewElement(htmlElements[3]);
 createNewElement(htmlElements[3]);
 console.log(elements);
+
+let elementsChatosos = [
+    {
+        id: "element_1",
+        tag: "div",
+        parentId: null,
+        attributes: {
+            class: "container",
+            style: "padding: 1rem;"
+        },
+        content: "",
+    },
+    {
+        id: "element_2",
+        tag: "h1",
+        parentId: "element_1",
+        attributes: {
+        },
+        content: "Hello World!",
+    },
+    {
+        id: "element_3",
+        tag: "p",
+        parentId: "element_1",
+        attributes: {
+        },
+        content: "Este é um parágrafo",
+    }
+];
+function renderElementById(id, elementsArray){
+    let resultado = "";
+    elementsArray.forEach(element => {
+        let linha = "";
+        const elementId = element.id;
+        const elementTag = element.tag;
+        const elementParentId = element.parentId;
+        const elementClass = element.attributes.class
+        const elementStyle = element.attributes.style
+        const childerns = elementsArray.filter(e => e.parentId === elementId)
+        const elementContent = element.content;
+
+        const Class = `class="${elementClass}"`
+        const Style = `style="${elementStyle}"`
+        if (elementClass && elementStyle){
+            linha = `<${elementTag} ${Class} ${Style}>`
+        }
+        else if (elementClass){
+            linha = `<${elementTag} ${Class}`
+        }
+        else if (elementStyle){
+            linha = `<${elementTag} ${Style}`
+        }
+        console.log(linha)
+        content.append(elementId)
+    });
+}
+renderElementById("element_1", elementsChatosos);
